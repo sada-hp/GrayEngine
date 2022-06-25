c4d0e3c9887f1ce2f058bc70c26379db54f77ec8
HarryP0ster <garryplakat@gmail.com>
2022-06-25 17:18:21 +0800

    cache

--- GrayEngine/Engine/Source/Vulkan/Shaders/Compiler.bat
+++
@@ -1,7 +0,0 @@
-C:/VulkanSDK/1.2.198.1/Bin/glslc.exe shader.vert -o vert.spv
-C:/VulkanSDK/1.2.198.1/Bin/glslc.exe shader.frag -o frag.spv
-copy D:\GrEngine\GrayEngine\GrayEngine\Engine\Source\Vulkan\Shaders\vert.spv D:\GrEngine\GrayEngine\bin\Debug-x64\SceneEditor\Shaders\
-copy D:\GrEngine\GrayEngine\GrayEngine\Engine\Source\Vulkan\Shaders\frag.spv D:\GrEngine\GrayEngine\bin\Debug-x64\SceneEditor\Shaders\
-copy D:\GrEngine\GrayEngine\GrayEngine\Engine\Source\Vulkan\Shaders\vert.spv D:\GrEngine\GrayEngine\bin\Release-x64\SceneEditor\Shaders\
-copy D:\GrEngine\GrayEngine\GrayEngine\Engine\Source\Vulkan\Shaders\frag.spv D:\GrEngine\GrayEngine\bin\Release-x64\SceneEditor\Shaders\
-pause
